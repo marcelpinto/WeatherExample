bccd732e9c7b5860ff8f32172f54e4de797ad6d9
Marcel Pinto Biescas <marcel.pintobiescas@here.com>
2018-04-30 11:02:54 +0200

    Handle API response and create input views.

--- app/src/main/java/com/here/example/weather/MainActivity.kt
+++ app/src/main/java/com/here/example/weather/MainActivity.kt
@@ -2,8 +2,8 @@ package com.here.example.weather
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.Button
-import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.experimental.Job
 import kotlinx.coroutines.experimental.android.UI
 import kotlinx.coroutines.experimental.launch
 
@@ -12,17 +12,42 @@ class MainActivity : AppCompatActivity() {
 
     private val weatherRepository = WeatherRepository()
 
+    private var job: Job? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.checkButton).setOnClickListener {
-            launch(UI) {
+        checkButton.setOnClickListener { button ->
+            job = launch(UI) {
                 // Show loading
-                val data = weatherRepository.getObservation(52.1, 13.5)
+                button.isEnabled = false
+                weatherInfo.text = "Loading..."
+
+                // Network call
+                val lat = latitudeInput.text.toString().toDouble()
+                val lng = longitudeInput.text.toString().toDouble()
+                val response = weatherRepository.getObservation(lat, lng)
+
                 // process the data on the UI thread
-                findViewById<TextView>(R.id.weatherInfo).text = data.toString()
+                val displayText = when (response) {
+                    is ApiResponse.Success<WeatherObservation> -> {
+                        response.result.observations.location.first().observation.first().run {
+                            "$description in $city with a temperature of ${temperature.toDouble().toInt()}°C" +
+                                    "\n\nMeta information: ${toString()}"
+                        }
+                    }
+                    is ApiResponse.Failure -> "Invalid call error: ${response.code}"
+                    is ApiResponse.Exception -> "There was an error ${response.throwable}"
+                }
+                weatherInfo.text = displayText
+                button.isEnabled = true
             }
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        job?.cancel()
     }
 }
--- app/src/main/java/com/here/example/weather/WeatherRepository.kt
+++ app/src/main/java/com/here/example/weather/WeatherRepository.kt
@@ -6,6 +6,7 @@ import kotlinx.coroutines.experimental.CommonPool
 import kotlinx.coroutines.experimental.withContext
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.lang.Exception
 import kotlin.coroutines.experimental.CoroutineContext
 
 class WeatherRepository {
@@ -24,10 +25,27 @@ class WeatherRepository {
         weatherApi = retrofit.create(WeatherApi::class.java)
     }
 
-    suspend fun getObservation(lat: Double, lng: Double): WeatherObservation {
+    suspend fun getObservation(lat: Double, lng: Double): ApiResponse<WeatherObservation, Any> {
         return withContext(context) {
-            weatherApi.getObservations("observation", lat, lng, true).execute().body()!!
+            try {
+                val response = weatherApi.getObservations("observation", lat, lng, true).execute()
+                if (response.isSuccessful && response.body() != null) {
+                    ApiResponse.Success(response.body()!!)
+                } else {
+                    ApiResponse.Failure<WeatherObservation, Any>(response.code(), response.errorBody())
+                }
+            } catch (e: Exception) {
+                ApiResponse.Exception<WeatherObservation, Any>(e)
+            }
         }
     }
+}
 
+sealed class ApiResponse<out T, out S> {
+
+    data class Success<out T>(val result: T) : ApiResponse<T, Any>()
+
+    data class Failure<out T, out S>(val code: Int, val errorBody: S? = null) : ApiResponse<T, S>()
+
+    data class Exception<out T, out S>(val throwable: Throwable) : ApiResponse<T, S>()
 }
